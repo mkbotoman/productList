120acd8c755f030f620b19e736739b761eedd9a2
mkbotoman <mkbotoman@gmail.com>
2015-02-28 16:33:14 -0800

    get table working and style

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,8 +6,8 @@ var productControllers = angular.module('productControllers', []);
 
 productControllers.controller('ProductListCtrl', ['$scope', 'Product',
   function($scope, Product) {
-    //$scope.products = Product.query();
-    //$scope.orderProp = 'age';
+    $scope.products = Product.query();
+    $scope.orderProp = 'name';
   }]);
 
 productControllers.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'Product',
