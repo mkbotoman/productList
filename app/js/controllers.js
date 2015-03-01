35feb3ee8df192509271bb036de082246961c5ad
mkbotoman <mkbotoman@gmail.com>
2015-03-01 08:44:41 -0800

    add column and row switch

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,6 +8,25 @@ productControllers.controller('ProductListCtrl', ['$scope', 'Product',
   function($scope, Product) {
     $scope.products = Product.query();
     $scope.orderProp = 'name';
+    $(".reverse").click(function(){
+      $("table").each(function() {
+        var $this = $(this);
+        var newrows = [];
+        $this.find("tr").each(function(){
+          var i = 0;
+          $(this).find("td").each(function(){
+            i++;
+            if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
+            newrows[i].append($(this));
+          });
+        });
+        $this.find("tr").remove();
+        $.each(newrows, function(){
+          $this.append(this);
+        });
+      });  
+      return false;
+    });
   }]);
 
 productControllers.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'Product',
